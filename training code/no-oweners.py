143d910dad40e58b0372e084bb5c4458e2698a37
Joe Meyrick <joemeyrick@gmail.com>
2017-09-28 14:17:31 -0400

    handling broken symlinks

--- training code/no-oweners.py
+++ training code/no-oweners.py
@@ -7,9 +7,16 @@ for user in pwd.getpwall():
 
 
 testdir = "DIR"
+
 for folder, dirs, files in os.walk(testdir):
     for files in files:
         path = folder + "/" + file
-        attributes = os.stat(path)
+
+        try:
+            attributes = os.stat(path)
+        except FileNotFoundError:
+            print(path + " not found")
+            continue
+
         if attributes.st_uid not in uidset
                 print(path + " has no owner")
