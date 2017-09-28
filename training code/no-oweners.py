668204248a036080a8fa745d79353f8a01c6c6f2
Joe Meyrick <joemeyrick@gmail.com>
2017-09-28 14:11:45 -0400

    inital commit

--- training code/no-oweners.py
+++ training code/no-oweners.py
@@ -1,1 +1,15 @@
+import pwd
 import os
+
+uidset = set()
+for user in pwd.getpwall():
+    uidset.add(user.pwd_uid)
+
+
+testdir = "DIR"
+for folder, dirs, files in os.walk(testdir):
+    for files in files:
+        path = folder + "/" + file
+        attributes = os.stat(path)
+        if attributes.st_uid not in uidset
+                print(path + " has no owner")
